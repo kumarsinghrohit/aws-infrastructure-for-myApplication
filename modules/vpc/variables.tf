345997e70a58d2856cc5aebf446fd87e79c24f2d
kumarsinghrohit <53214059+kumarsinghrohit@users.noreply.github.com>
2021-07-11 01:49:26 +0545

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,1 +1,20 @@
+variable "name" {
+  description = "name"
+}
+
+variable "cidr" {
+  description = "VPC id"
+}
+
+variable "public_subnets" {
+  description = "subnets used for myApplication"
+}
+
+variable "private_subnets" {
+  description = "private subnets without external communication"
+}
+
+variable "availability_zones" {
+  description = "availability zones"
+}
 
