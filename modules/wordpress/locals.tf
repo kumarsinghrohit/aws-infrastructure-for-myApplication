bc15e9a5c570e6581c942db907e0f449022632f0
kumarsinghrohit <53214059+kumarsinghrohit@users.noreply.github.com>
2021-08-15 11:51:07 +0200

    Create locals.tf

---
+++ modules/wordpress/locals.tf
@@ -0,0 +1,8 @@
+locals {
+  spot_price = {
+    "t3.small"    = "0.0095" # 0.024 on demand
+    "t3a.micro"  = "0.0035" # 0.0432 on demand
+    "t3a.medium"  = "0.0120" # 0.0432 on demand
+    "t3a.nano"    = "0.003" # 0.0054 on demand
+  }
+}
